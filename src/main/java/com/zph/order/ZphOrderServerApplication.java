723bfd0b4981bc96d2561cd0d452bf629d5fcafc
zhangpenghui <zhangpenghui>
2018-10-02 09:40:11 +0800

    fisrt

---
+++ src/main/java/com/zph/order/ZphOrderServerApplication.java
@@ -0,0 +1,12 @@
+package com.zph.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZphOrderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZphOrderServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zph/order/ZphOrderServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zph.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZphOrderServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
